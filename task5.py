56f1823bd5295f98428cffb65f16f60dd6730cc1
Nikiforov Viacheslav <55716893+slabuke@users.noreply.github.com>
2020-03-03 10:00:56 +0300

    2-d fix (x += 1 etc.)

--- task5.py
+++ task5.py
@@ -4,11 +4,11 @@ x = 0
 y = 0
 for m in moves:
     if m == 'L':
-        x = x - 1
+        x -= 1
     elif m == 'R':
-        x = x + 1
+        x += 1
     elif m == 'U':
-        y = y + 1
+        y += 1
     else:
-        y = y - 1
+        y -= 1
 print(x == 0 and y == 0)
