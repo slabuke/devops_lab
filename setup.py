2e28e2f8db224ae36ed6904c70b3b5b2daf4dcc6
Viachaslau <student@localhost.localdomain>
2020-03-03 21:16:09 +0300

    1-st commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+import setuptools
+
+setuptools.setup(
+    name="snapshot",
+    packages=setuptools.find_packages(),
+    scripts=['snapshot'],
+    version="1.0",
+    author="Nikifarau Viachaslau",
+    author_email="viachaslau_nikifarau@emap.com",
+    py_modules=['snapshot'],
+    description="Snapshot app",
+)
