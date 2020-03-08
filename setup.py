6cf246895cb19027a637f4a9757d46ae472f8a4b
Nikiforov Viacheslav <55716893+slabuke@users.noreply.github.com>
2020-03-08 14:42:49 +0300

    Snapshot.py fixed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name="snapshot",
     packages=setuptools.find_packages(),
-    scripts=['snapshot'],
+    scripts=['snapshot.py'],
     version="1.0",
     author="Nikifarau Viachaslau",
     author_email="viachaslau_nikifarau@emap.com",
